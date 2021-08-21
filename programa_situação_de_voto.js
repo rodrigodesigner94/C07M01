7e49106f27084dfd234c922fd4882b393924e3f6
Rodrigo Borges <85399193+rodrigodesigner94@users.noreply.github.com>
2021-08-20 23:56:27 -0300

    Programa de verificação de voto
    Programa de verificação de elegibilidade de voto mediante entrada de dados (ano de nascimento e ano da eleição)

---
+++ programa_situação_de_voto.js
@@ -0,0 +1,24 @@
+
+
+console.log("Consulta de situação de voto");
+
+const ano = +prompt("Informe o ano de nascimento: ");
+const anoRef = +prompt("Informe o ano atual: ");
+
+function resultado(anoRef, ano) {
+  let calculo = (anoRef - ano)
+  console.log(`Você tem ${calculo}`)
+  if (calculo < 16){
+    console.log("Voto Negado")
+  } 
+   else if (calculo >= 18){
+    console.log("Voto Obrigatorio")
+  } 
+  else if (calculo === 17){
+    console.log("Voto Opcional")
+  }
+  else if (calculo === 16){
+    console.log("Voto Opcional")
+  } else {}
+}
+resultado(anoRef, ano)
